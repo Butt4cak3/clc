c0d9ca205205193d9a5899ed7997e6a961bc9a75
Marius Becker <marius.becker.8@gmail.com>
2021-04-03 02:17:34 +0200

    Change main program to a REPL

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use clc::{evaluate, Context};
 use core::f64::consts::PI;
+use std::io::{self, Write};
 
 fn main() {
     let mut context = Context::default();
     context.set_variable("pi", PI);
 
-    let expression = "2 * pi";
-    let result = evaluate(expression, &context);
+    loop {
+        let mut input = String::new();
+        print!("> ");
+        io::stdout().flush().expect("Input error");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    println!("{} = {}", expression, result);
+        if input == "" {
+            println!("");
+            break;
+        } else if input.trim() == "" {
+            continue;
+        }
+
+        let result = evaluate(input.trim(), &context);
+        println!("{}\n", result);
+    }
 }
